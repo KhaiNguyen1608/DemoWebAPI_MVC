acbdecac9a70a40352bc5925361de87f7c507eca
KhaiNguyen1608 <38730340+KhaiNguyen1608@users.noreply.github.com>
2020-03-25 17:47:12 +0700

    Update CRUDDB.sql

--- WebApiInMVC/CRUDDB.sql
+++ WebApiInMVC/CRUDDB.sql
@@ -1,4 +1,4 @@
-create database CRUDDB
+﻿create database CRUDDB
 use CRUDDB
 
 CREATE TABLE [dbo].[Employee](
@@ -8,4 +8,31 @@ CREATE TABLE [dbo].[Employee](
 	[Age] [int] NULL,
 	[Salary] [int] NULL)
 
-select * from Employee
+
+create table City
+(
+	[CityID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	CityName nvarchar(50) null
+)
+create table District
+(
+	[DistrictID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	DistrictName nvarchar(50) null
+)
+create table Ward
+(
+	[WardID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	WardName int null
+)
+
+insert into City(CityName) values(N'Hồ Chí Minh')
+insert into City(CityName) values(N'Đã Nẵng')
+insert into City(CityName) values(N'Thanh Hóa')
+insert into City(CityName) values(N'Hà Nội')
+insert into City(CityName) values(N'Đà Lạt')
+
+insert into District(DistrictName) values(N'Gò Vấp')
+insert into District(DistrictName) values(N'Bình Thạnh')
+
+insert into Ward(WardName) values(16)
+insert into Ward(WardName) values(17)
